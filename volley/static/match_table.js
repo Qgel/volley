9c27c62045faf0e64d652bc5765ed90eddc48130
Mirko Kugelmeier <kugelmeier@fka.de>
2018-08-15 17:45:35 +0200

    Fix pagination when more than 10 pages of games are added

--- volley/static/match_table.js
+++ volley/static/match_table.js
@@ -6,41 +6,13 @@ $('.delete-button')
     })
 ;
 
+
 $(document).ready(function () {
-    var rowsShown = 10;
-    var rowsTotal = $('#data tbody tr').length;
-    var numPages = rowsTotal / rowsShown;
-    for (i = 0; i < numPages; i++) {
-        var pageNum = i + 1;
-        $('#t-next').before('<a href="#/" class="item num" rel="' + i + '">' + pageNum + '</a> ');
-    }
-    $('#data tbody tr').hide();
-    $('#data tbody tr').slice(0, rowsShown).show();
-    $('.ui.right.floated.pagination.menu a.num:first').addClass('active');
-    $('.ui.right.floated.pagination.menu a.num').bind('click', function () {
-
-        $('.ui.right.floated.pagination.menu a.num').removeClass('active');
-        $(this).addClass('active');
-        var currPage = $(this).attr('rel');
-        var startItem = currPage * rowsShown;
-        var endItem = startItem + rowsShown;
-        $('.ui.right.floated.pagination.menu a.icon').attr('rel', currPage);
-        $('#data tbody tr').css('opacity', '0.0').hide().slice(startItem, endItem).css('display', 'table-row').animate({opacity: 1}, 300);
-    });
-
-    $('.ui.right.floated.pagination.menu a.icon').bind('click', function () {
-        var currPage = Number($(this).attr('rel'));
-        var newPage;
-        if ($(this).attr('id') == 't-next') {
-            newPage = (currPage + 1).toString();
-        } else {
-            newPage = (currPage - 1).toString();
-        }
-
-        var b = $('.ui.right.floated.pagination.menu a.num[rel="' + newPage + '"]');
-        if (b.length > 0) {
-            $('.ui.right.floated.pagination.menu a.icon').attr('rel', newPage);
-            b.trigger('click');
-        }
+    $('#data tbody').paginathing({
+        perPage: 10,
+        limitPagination: 7,
+        insertAfter: '.paginator',
+        ulClass: 'ui right floated pagination menu',
+        liClass: 'item'
     });
 });
